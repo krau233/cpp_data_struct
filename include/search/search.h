d36e3b723e43f258d253d246602ec20e6a49040b
chenyixuan <1406947724@qq.com>
2024-01-25 10:48:46 +0800

    feat:折半查找

--- include/search/search.h
+++ include/search/search.h
@@ -18,4 +18,17 @@ int search_seq(CArrayList<T> array,T key){
     return 0;
 }
 
+//折半查找
+template<typename T>
+int binary_search(CArrayList<T> array,T key){
+    int low=1,high=array.length;
+    int mid;
+    while(low<=high){
+        mid = (low+high)/2;
+        if (array[mid]== key) return mid;
+        else if(array[mid]<key) low = mid+1;
+        else  high = mid-1;
+    }
+    return 0;
+}
 #endif //DATASTRUCT_SEARCH_H
