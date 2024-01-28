b7f2d9dac76393a6949c867126c575da27f8dc94
chenyixuan <1406947724@qq.com>
2024-01-28 17:49:51 +0800

    feat:二叉排序树的构造和使用

--- include/search/search.h
+++ include/search/search.h
@@ -7,6 +7,8 @@
 
 #include "../list/array.h"
 #include "../../src/list/array.cpp"
+#include "../tree/BiTree.h"
+#include "../../src/tree/BiTree.cpp"
 
 
 //顺序查找
@@ -32,6 +34,36 @@ int binary_search(CArrayList<T> array,T key){
         else  high = mid-1;
     }
     return 0;
+}
+
+//二叉排序树的查找
+template<typename T>
+BiTNode<T> *BST_Search(BiTree<T> t,T key){
+    BiTNode<T>* p = t.getRoot();
+    while(p!= nullptr&&p->getData()!=key){
+        if(key<p->getData()) p = p->getLchild();
+        else p = p->getRchild();
+    }
+    return p;
+}
+
+//二叉排序树的插入
+template<typename T>
+bool BST_Insert(BiTNode<T>* &p,T key){
+    if(p == nullptr){
+        p = new BiTNode<T>(key);
+        return true;
+    }
+    else if(key==p->getData()) return false;
+    else if(key<p->getData()) return BST_Insert(p->getLchild(),key);
+    else return BST_Insert(p->getRchild(),key);
+}
+
+//构造二叉排序树
+template<typename T>
+void Creat_BST(BiTree<T> t,T arr[],int n){
+    t.setRoot(nullptr);
+    for(int i = 0 ;i<n;++i) BST_Insert(t.getRoot(),arr[i]);
 }
 
 
